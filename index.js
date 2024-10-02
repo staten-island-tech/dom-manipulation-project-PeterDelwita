3382bb04a727f8483026fa8be82384b628dccece
PeterDelwita <peterd72@nycstudents.net>
2024-10-01 23:54:20 -0400

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+const DOMSelectors = {
+  header: document.querySelector("h1"),
+  cardheader: document.querySelector("card-header"),
+  form: document.querySelector("form"),
+  item: document.querySelector("li"),
+  button: document.querySelector("button"),
+  list: document.querySelector("ul"),
+};
