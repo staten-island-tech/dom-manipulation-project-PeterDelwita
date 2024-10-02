31d5f6c1932639f75561427ca77a4aae61cd3ada
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-10-02 12:52:38 -0400

    form, css

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const DOMSelectors = {
   header: document.querySelector("h1"),
-  cardheader: document.querySelector("card-header"),
+  cardheader: document.querySelector(".card-header"),
   form: document.querySelector("form"),
   item: document.querySelector("li"),
   button: document.querySelector("button"),
